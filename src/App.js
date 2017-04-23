95c769017e459e2667c9cac1039b1a3d2b9794d9
samarthbhargav <samarth.bhargav92@gmail.com>
2017-04-23 19:24:38 +0530

    Added map + geo-json

--- src/App.js
+++ src/App.js
@@ -2,32 +2,26 @@ import React, { Component } from 'react';
 import {axios} from './http_api.js';
 import './App.css';
 import  govtData from "./govtData.csv";
+import {DengueMap} from "./map.js";
 
 class App extends Component {
 
   App() {
     console.log("Constructor!");
-
   }
 
   componentWillMount(){
-
     this.state = {};
-    axios.get(govtData).then(function(response) {
-      console.log(response.data);
-      this.setState({"csv": response.data});
-    }.bind(this)).catch(function(error) {
-      console.log(error);
-    });
   }
 
 
   render() {
     var data = this.state.csv || {};
     return (
-      <div className="App">
-        <p>{data.toString()}</p>
+      <div id="map_container">
+        <DengueMap />
       </div>
+
     );
 
   }
